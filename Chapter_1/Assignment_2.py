66faaa5b6571d8592090151e5a4b12d6f37343ab
Rajan-91480 <rajan91480@gmail.com>
2026-07-23 22:33:30 +0530

    chatper 1 Assignment_2.py file added and completed

---
+++ Chapter_1/Assignment_2.py
@@ -0,0 +1,11 @@
+# assignment 1 is to create a code for printing the hellow world 10 times use of the this symbole "*"
+
+# Assignment 2
+
+# Q- Take diameter as input and calculate the area of circle and print the result
+
+diameter = input("Enter the Diameter of the circle : ")
+radius = int(diameter) / 2
+area = 3.14 * (radius ** 2)
+print("this is the result of the radius of the circle : ", radius)
+print("this is the area of the circle result : ", area)
