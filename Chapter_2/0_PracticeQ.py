2230414e17825b821b3814269738b0561dfbf7b4
Rajan-91480 <rajan91480@gmail.com>
2026-07-24 22:41:57 +0530

    PracticeQ.py file has created to practice the program

---
+++ Chapter_2/0_PracticeQ.py
@@ -0,0 +1,5 @@
+# program to take age as imput and print value enterd and its data type
+
+age = int(input("Enter your Age : "))   # taking input from user and converting it into integer data type
+print("Value Entered : ", age)          # printing the value entered by the user
+print(type(age))                        # printing the data type of age variable
