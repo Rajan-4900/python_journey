6c692e012e1a890997e6c6d9b23ab252edd551e1
Rajan-91480 <rajan91480@gmail.com>
2026-07-25 22:49:52 +0530

    practice questions is done for type convertion

---
+++ Chapter_2/2.1_PracticeQ.py
@@ -0,0 +1,10 @@
+# take number as input and convert to float and print bothe original and converted value with your data types
+
+num1 = int(input("Enter the 1st Number : "))
+num2 = float(num1)
+
+print("original value is : ", num1)
+print("The Data Type Is : ", type(num1))
+
+print("Converted Value is : ", num2)
+print("The Data Type Is : ", type(num2))
