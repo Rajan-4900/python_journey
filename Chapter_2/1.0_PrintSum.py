4e8729bd364ad0cc789678efb7a22d115a7eaa31
Rajan-91480 <rajan91480@gmail.com>
2026-07-25 22:26:04 +0530

    new file is added in the chapter 2 for finding sum and avg

---
+++ Chapter_2/1.0_PrintSum.py
@@ -0,0 +1,14 @@
+# explain program to input two numbers and print their sum :
+
+input1 = int(input("Enter the 1st input number : "))
+input2 = int(input("Enter the 2nd input number : "))
+
+sum = input1 + input2
+
+print("Sum of two numbers is : ", sum)
+
+# modify and find the average of the two numbers instead of Sum :
+
+avg = sum/2
+
+print("The Average of two number is : ", avg)
