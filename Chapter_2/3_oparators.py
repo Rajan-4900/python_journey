74a8327dc1cf88988ddbef71e87d47859c8d6f18
Rajan-91480 <rajan91480@gmail.com>
2026-08-06 21:52:44 +0530

    new file added in chapter 2 with the name called operator.py

---
+++ Chapter_2/3_oparators.py
@@ -0,0 +1,13 @@
+# arithmetic operators
+
+x = 10
+y = 5
+
+print("Sum :", x + y)
+print("Diff :", x - y)
+print("Product :", x * y)
+print("Division :", x / y)
+print("Modulus :", x % y)
+print("Exponent :", x ** y)
+
+# Comparition operators
