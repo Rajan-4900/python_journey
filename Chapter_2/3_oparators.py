0c17c0f6265b9a2d336259ad29ecba3c0925e5d4
Rajan-91480 <rajan91480@gmail.com>
2026-08-06 22:02:55 +0530

    New Code Added To The operator.py file in chpt 2

--- Chapter_2/3_oparators.py
+++ Chapter_2/3_oparators.py
@@ -1,8 +1,6 @@
 # arithmetic operators
-
 x = 10
 y = 5
-
 print("Sum :", x + y)
 print("Diff :", x - y)
 print("Product :", x * y)
@@ -10,4 +8,16 @@ print("Division :", x / y)
 print("Modulus :", x % y)
 print("Exponent :", x ** y)
 
-# Comparition operators
+print()
+
+# Comparison operators
+print("Equal :", x == y)
+print("Greater than :", x > y)
+print("Less than :", x < y)
+print("Greater than or equal :", x >= y)
+print("Less than or equal :", x <= y)
+print("Not equal :", x != y)
+
+print()
+
+# Logical Operators
