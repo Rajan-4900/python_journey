5c12fa79325afeab42882b6bdb1368f082e4edf7
Rajan-91480 <rajan91480@gmail.com>
2026-08-07 22:57:56 +0530

    the practice question is done in chpt 3 for str indexing

---
+++ Chapter_3/1.1_PracticeQ.py
@@ -0,0 +1,11 @@
+# write a python program that takes a user's name as input and print:
+    # the first char
+    # the last char
+    # the total length of the name
+
+# str_name = input("enter the name: ")
+str_name = "Rajan"
+
+print("First Char: ", str_name[0])  #R
+print("Last Char: ", str_name[4])   #n
+print("Total length of the char: ", len(str_name))  #5
