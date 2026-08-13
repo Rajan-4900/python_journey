352173146b6bfc4be600adde8e507405565971b7
Rajan-91480 <rajan91480@gmail.com>
2026-08-13 22:17:45 +0530

    formated String file is create in chpt 3

---
+++ Chapter_3/4_FormatedStr.py
@@ -0,0 +1,9 @@
+# f-string makes it easy to include variables inside strings.
+
+car = "BMW"
+model_Num = 7
+print(f"This Is {car} car and this is the i{model_Num} Model Number")
+
+name = "Rajan"
+age = 21
+print(f"My Name is {name} and I am {age} year old")
