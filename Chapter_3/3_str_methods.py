afe706760114458a5642236a2830d67c77001961
Rajan-91480 <rajan91480@gmail.com>
2026-08-13 21:56:12 +0530

    new string method file created in chpt 3

---
+++ Chapter_3/3_str_methods.py
@@ -0,0 +1,11 @@
+# Common String Methods 
+
+str = "python world"
+
+print(str.upper())      # Upper Case Letter------> PYTHON
+print(str.lower())      # Lower case Letter------> python
+print(str.capitalize()) # 1st letter capital and remaining will be small ------> Python
+print(str.title())      # it will capital all word 1st letter-----> Python World
+print(str.find("ho"))   # it will find the place of the lleter in index value ---> 3
+print(str.replace("world","prg"))    # it will replace the charactor or word or letter--> replace->"world"<--->"prg"
+print(str.count("o"))      # this will count the number of occurrence in the string--> o -> 2
