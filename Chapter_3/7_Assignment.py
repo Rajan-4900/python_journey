626f30b6fc8b5ca7032cc15751eaa802fa16c0d4
Rajan-91480 <rajan91480@gmail.com>
2026-08-13 23:09:50 +0530

    assignment 2nd question done in chapter 3

--- Chapter_3/7_Assignment.py
+++ Chapter_3/7_Assignment.py
@@ -1,9 +1,15 @@
-# write  a program that takes a sentence as input and print:
+# Q1: write  a program that takes a sentence as input and print:
     # Total characters(len())
     # UpperCase Version 
     # LowerCase Version
     # Add Some Emoji In Between
 
+# Q2: write a python program that takes any word or sentence as input and print:
+    # the 1st character
+    # the last character
+    # total Number of character
+
+# Q1
 ass = input("Enter A Sentence: ")
 
 ass = ass.replace(":)", "🙂")
@@ -17,3 +23,20 @@ print("Length Of the Sentence: ", len(ass))
 print("Upper Case: ", ass.upper())
 print("Lower Case: ", ass.lower())
 
+print()
+# Q2
+
+user = input("Enter the sentence: ")
+
+split = user.split()
+first_char = split[0]
+mid_char = len(split)//2
+mid = split[mid_char]
+last_char = split[-1]
+
+print("Splited Char: ",split)
+print("1st Char: ",first_char)
+print("Mid Char: ", mid_char)
+print("last Char: ",last_char)
+print("Length Befor Update: ",len(user))
+print("Length After Update: ",len(split))
