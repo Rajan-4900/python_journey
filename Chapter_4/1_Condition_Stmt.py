ea3c83a009febc82e67173b175b953fa68c72976
Rajan-91480 <rajan91480@gmail.com>
2026-08-14 22:47:30 +0530

    new chapter-4 created and conditional statemtn file is created

---
+++ Chapter_4/1_Condition_Stmt.py
@@ -0,0 +1,20 @@
+# Conditional Statemts :
+    # if
+    # if-else
+    # elif
+
+marks = 70
+
+if (marks >= 90):
+    print("Your Grade Is A")
+elif(marks >= 80):
+    print("Your Grade is B")
+
+print()
+
+age = 21
+
+if(age >= 18):
+    print("You are eligible to Vote")
+else:
+    print("You Are Not eligible to vote")
