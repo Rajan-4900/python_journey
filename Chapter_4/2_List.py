9092a2dc0f1147b04a6cdb1f6342cdb8a7404cc9
Rajan-91480 <rajan91480@gmail.com>
2026-08-16 23:19:34 +0530

    new code updated in list in chpt 4

--- Chapter_4/2_List.py
+++ Chapter_4/2_List.py
@@ -6,3 +6,8 @@ print(len(food))
 print("First Value OF the List: ", food[0])
 print("Last Value Of the List: ", food[-1])
 
+print()
+
+middle_index = len(food)//2
+output = food[middle_index]
+print("Midle Valuye Of the List: ", output)
