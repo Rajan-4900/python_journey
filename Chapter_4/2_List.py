42daedd4a39b7b000f9dffca83686bbe21324936
Rajan-91480 <rajan91480@gmail.com>
2026-08-16 23:14:44 +0530

    new lists topic is going in chpt 4

---
+++ Chapter_4/2_List.py
@@ -0,0 +1,8 @@
+# List Programming
+
+food = ["Samosa", "Poori", "Masala Dhosa", "MasalPuri", "PaniPuri"]
+
+print(len(food))
+print("First Value OF the List: ", food[0])
+print("Last Value Of the List: ", food[-1])
+
