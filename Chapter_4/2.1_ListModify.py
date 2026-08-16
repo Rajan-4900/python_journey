7e72200e3f9ae3c15fda93546ecef230c32c330d
Rajan-91480 <rajan91480@gmail.com>
2026-08-16 23:43:04 +0530

    modification is updated in the list of food to check in chpt 4

--- Chapter_4/2.1_ListModify.py
+++ Chapter_4/2.1_ListModify.py
@@ -6,3 +6,13 @@ print("Beforer List: ", numList)
 
 numList[4] = 34     # 43 --> 34
 print("After List: ", numList)
+
+# we can change string also 
+food = ["Samosa", "Poori", "Masala Dhosa", "MasalPuri", "PaniPuri"]
+
+# Before
+print("Before the Food List: ", food)
+
+# After
+food[1] = "Noodles"
+print("After the Modification Of the food List: ", food)
