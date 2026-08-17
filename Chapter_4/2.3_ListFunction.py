2ca944f1cb054c4f87448eed61c870f3b60a847b
Rajan-91480 <rajan91480@gmail.com>
2026-08-17 23:14:30 +0530

    add some list functions in chpt 4

---
+++ Chapter_4/2.3_ListFunction.py
@@ -0,0 +1,10 @@
+# List Functions
+
+marks = [43, 65 ,32, 87 , 83, 21, 90]
+
+# Find the length of the list
+print(len(marks))
+
+# find the min ad mix from the give list
+print("Max Marks: ",max(marks))       # 90
+print("Min Marks: ",min(marks))       # 21
