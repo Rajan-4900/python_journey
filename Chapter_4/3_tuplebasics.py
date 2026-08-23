0dd89a637b71ce01ab7429de635958f4c7e003c1
Rajan-91480 <rajan91480@gmail.com>
2026-08-23 22:49:39 +0530

    new tuple is arrived in chpt 4

---
+++ Chapter_4/3_tuplebasics.py
@@ -0,0 +1,14 @@
+# Tuple Basics
+
+myTuple = (23, 76, 87, 43, 54, 67, 54)
+std_tuple = ("raj", "tuple", "laptop")
+
+
+# std_tuple[1] = "raj"
+print(std_tuple[2])
+
+print(type(std_tuple))
+
+# Single tuple
+std_single_tuple = (1)
+print(std_single_tuple)
