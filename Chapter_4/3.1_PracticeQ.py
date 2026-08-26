199bc8af03e687a0b11f46afbaff3672c232377e
Rajan-91480 <rajan91480@gmail.com>
2026-08-26 22:29:21 +0530

    new practice file addded for tuple in chpt 4

---
+++ Chapter_4/3.1_PracticeQ.py
@@ -0,0 +1,7 @@
+# Create a tuple of your favorite 5 fruits. Then Print
+    # The Total number of fruits
+    # The index of one selected fruit
+
+fruit = ("mango", "apple", "watermelon", "leachi", "orange")
+print("The Total Number Of Fruits: ", len(fruit))
+print("The Index Of WaterMelon Is : ", fruit.index("watermelon"))
