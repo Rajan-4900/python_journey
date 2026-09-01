fe24f6c6a30a0188addb9f326e818e2f600d1d97
Rajan-91480 <rajan91480@gmail.com>
2026-09-01 22:04:12 +0530

    code is updated in dictBasic.py file in chapt 5

--- Chapter_5/1_dictBasics.py.py
+++ Chapter_5/1_dictBasics.py.py
@@ -20,3 +20,22 @@ print(student)
 student["Goal"] = "Software Engineer"
 print(student)
 
+# Removing the Item
+student.pop("College")
+print(student)
+
+# Return All Keys
+print(student.keys())  # dict_keys(['name', 'Age', 'City', 'Goal'])
+
+# return All Values
+print(student.values()) # dict_values(['Rajan', 21, 'Hyderabad', 'Software Engineer'])
+
+# return all key-vlaue pairs as tuples
+print(student.items())  # dict_items([('name', 'Rajan'), ('Age', 21), ('City', 'Hyderabad'), ('Goal', 'Software Engineer')])
+
+# return value of a key safely
+print(student.get("name"))      #Rajan
+
+# Updates Dictionary with another dictionary
+student.update({"Age": 22, "City": "Chennai"})      
+print(student)  # {'name': 'Rajan', 'Age': 22, 'City': 'Chennai', 'Goal': 'Software Engineer'}
