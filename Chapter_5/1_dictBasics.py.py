c0d9af22315b446bf9a17a10c8b685563ea0e293
Rajan-91480 <rajan91480@gmail.com>
2026-09-01 21:18:24 +0530

    dictBasic.py file updated and added new codes in chpt 5

--- Chapter_5/1_dictBasics.py.py
+++ Chapter_5/1_dictBasics.py.py
@@ -8,3 +8,15 @@ student = {
 }
 
 print(type(student))  # <class 'dict'>
+print(student["name"])      # Rajan
+
+print(student)      # {'name': 'Rajan', 'Age': 21, 'City': 'Bangalore', 'College': 'XYZ Clg'}
+
+# Updating the values
+student["City"] = "Hyderabad"
+print(student)
+
+# Adding the new key-value pair
+student["Goal"] = "Software Engineer"
+print(student)
+
