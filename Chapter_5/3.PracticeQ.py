51d913808a482ddcd3a97fa8f9e741e8c8203054
Rajan-91480 <rajan91480@gmail.com>
2026-09-11 23:32:27 +0530

    3.practiceQ.py file added in the chapter 5

---
+++ Chapter_5/3.PracticeQ.py
@@ -0,0 +1,16 @@
+# You are given a list of Programming langues
+# ["Python", "Java", "C++", "Python", "Java", "C", "ReactJS"]
+# Convert it into a set  and print how many unique languages luffy knows
+
+prg_lang = ["Python", "Java", "C++", "Python", "Java", "C", "ReactJS"]
+print("Length of Prg_lang is: ", len(prg_lang))
+print(type(prg_lang))
+
+print()
+print("Converted list int Set : ")
+print()
+
+prg_set = set(prg_lang)
+print(prg_set)
+print(type(prg_set))
+print("Length of Prg_set is: ", len(prg_set))
