86340cc0f709925dc1973c44ec60d71d3b21fd09
Rajan-91480 <rajan91480@gmail.com>
2026-09-13 22:36:01 +0530

    serial number with name printing in python done in chapter 6 in 1.5 Practice question

---
+++ Chapter_6/1.5Q_num_with_name.py
@@ -0,0 +1,8 @@
+# Print a name 5 times , but each time with a number in front of it . 
+# write a program using a while loop that prints.
+
+n = 1
+
+while n <= 5:
+    print(n,"Rajan L")
+    n += 1
