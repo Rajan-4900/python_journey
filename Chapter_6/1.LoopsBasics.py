6126c8e91e9abf53667c6e0114950876b8e541b2
Rajan-91480 <rajan91480@gmail.com>
2026-09-13 19:39:02 +0530

    new loop basics to advance chapter is create with 1.loopBasic.py

---
+++ Chapter_6/1.LoopsBasics.py
@@ -0,0 +1,14 @@
+# Loops in Python are used to execute a block of code repeatedly as long as a certain condition is met. 
+# There are two main types of loops in Python: `for` loops and `while` loops.
+
+# 1. While Loop : A while loop runs as long as a condition is true. 
+# It is used when the number of iterations is not known beforehand.
+
+num = 1
+while num <= 10:
+    print("Hello World")
+    num = num + 1  # Increment the counter to avoid infinite loop
+
+print("Now We Printed the Msg 10 times")
+
+
