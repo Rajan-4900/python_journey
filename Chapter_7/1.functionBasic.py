7b8d7669e72b6fc513e5bfa29fdc1831d78ae9ce
Rajan-91480 <rajan91480@gmail.com>
2026-09-25 22:34:37 +0530

    new chapter 7 and functionBasics.py file created

---
+++ Chapter_7/1.functionBasic.py
@@ -0,0 +1,16 @@
+# Functiona Basics
+
+# what is function
+# A function in Python is a self-contained, reusable block of reusable code designed to perform a specific task.
+# instead of writing the same lines repeatedly, we define a function once and use it multiple times and anywhere 
+# for EXAMPLE
+
+# creating function 
+def sumFunc():      # created the function  def--> define name->sumFunc ()---> default
+    a = 5
+    b = 6
+    sum = a + b
+    print(sum)
+
+sumFunc()       # calling the Function
+
