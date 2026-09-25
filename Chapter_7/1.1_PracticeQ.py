4206083fcb18bf5ebc30c42a165c5d6393d82efa
Rajan-91480 <rajan91480@gmail.com>
2026-09-25 23:04:48 +0530

    new practice question added in chapter 7

---
+++ Chapter_7/1.1_PracticeQ.py
@@ -0,0 +1,8 @@
+# 1Q. Write a function named as welcom_message() that prints "Welcome to Python Programming!" three times.
+
+def welcome_message():
+    print("Welcom To Python Programming!")
+
+welcome_message()
+welcome_message()
+welcome_message()
