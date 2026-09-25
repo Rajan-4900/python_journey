ddc36baf6a9efaceb52eb0c4f9f56f041b7f2bb1
Rajan-91480 <rajan91480@gmail.com>
2026-09-25 23:05:53 +0530

    new 2nd question is done in 1.1practiceQ.py

--- Chapter_7/1.1_PracticeQ.py
+++ Chapter_7/1.1_PracticeQ.py
@@ -1,8 +1,14 @@
 # 1Q. Write a function named as welcom_message() that prints "Welcome to Python Programming!" three times.
-
 def welcome_message():
     print("Welcom To Python Programming!")
 
 welcome_message()
 welcome_message()
 welcome_message()
+
+# 2Q. Define a function inspire() that prints a moticational quote with your name.
+def inspire():
+    print("Rajan Is Inspired from sowmya di to become a software engineer")
+
+inspire()
+
